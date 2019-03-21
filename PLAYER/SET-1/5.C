1ffa27480a2679b97ed1156920420355e0c12f37
Nithin1k <46814016+Nithin1k@users.noreply.github.com>
2019-03-21 16:10:05 +0530

    Create 5.C

---
+++ PLAYER/SET-1/5.C
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  char str[10];
+  int i,num=0,l;
+  scanf("%s",str);
+  l=strlen(str);
+  for(i=0;i<=l;i++)
+  {
+    if(str[i]=='I'||str[i]=='i')
+    {
+      num+=1;
+    }
+    else if(str[i]=='V'||str[i]=='v')
+    {
+      num+=5;
+    }
+    else if(str[i]=='X'||str[i]=='x')
+    {
+      num+=10;
+    }
+  }
+  for(i=l;i>0;i--)
+  {
+    if(str[i]>str[i-1])
+    {
+      num-=2;
+    }
+  }
+  printf("%d",num);
+ }
